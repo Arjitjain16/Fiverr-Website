b339170ce9e2415636c9938e1f524a1b9a340a44
Your Name <you@example.com>
2024-10-21 22:44:31 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+
+
+const Navbar = () => {
+    const isActive = ()=>{
+        return window.scrollY > 0? setActive(true) : setActive(false);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', isActive)
+        return () => window.removeEventListener('scroll', isActive)
+    }, [])
+    
+    const [active, setActive] = useState(false)
+    const [open, setOpen] = useState(false)
+    
+    const currentUser = {
+        id: 1,
+        username: 'tevas',
+        isSeller : true
+    }
+  return (
+   <div className={active ? `bg-white text-black` : `w-full flex flex-col bg-[#113118] text-white`}>
+     <div className="flex items-center justify-between py-3 px-48" style={{transition : `0.8s all ease`}}>
+        <h1 className="text-2xl font-bold">Fiverr.</h1>
+        <div className="flex items-center gap-5 font-semibold text-sm">
+            <h3>Fiverr Business</h3>
+            <h3>Explore</h3>
+            <h3>English</h3>
+            <button>Sign In</button>
+            {!currentUser?.isSeller && <h3>Become a Selller</h3>}
+           {!currentUser && <button className="border border-white rounded px-3 py-1">Join</button> }
+           {currentUser && (
+            <div className="flex items-center gap-3 relative" onClick={()=>setOpen(!open)}>
+                <img src="https://example.com/profile_pic.jpg" alt="Profile Pic" className="w-8 h-8 rounded-full cursor-pointer"/>
+                <span>{currentUser.username}</span>
+                {open && (<div className="flex flex-col gap-1 absolute top-11 right-0 bg-white text-gray-300 py-5 px-5 w-44 rounded">
+                    {currentUser?.isSeller && (
+                         <>
+                         <span>Gigs</span>
+                         <span>Add a new Gigs</span>
+                        </> 
+                    )}
+                    <span>Order</span>
+                    <span>Messages</span>
+                    <span>Logout</span>
+                </div>)}
+            </div>
+           )}
+            
+        </div>
+      </div>
+      {
+        active && (
+        <>
+           <hr className="w-full border-[0.2px] border-gray-200" style={{}}/>  
+            <div className="flex item-center justify-between px-48">
+                <span>test1</span>
+                <span>test2</span>
+            </div>
+        </>
+      )}
+   </div>
+  )
+}
+
+export default Navbar
