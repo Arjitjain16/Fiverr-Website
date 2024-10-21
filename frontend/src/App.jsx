dcd4b4cd0be487957443b85343d8efc10f4e1454
Your Name <you@example.com>
2024-10-21 23:40:03 +0530

    Routing commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import Navbar from "./components/Navbar"
-
+// import Navbar from "./components/Navbar"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login" 
+import Gig from "./pages/Gig"
+import Gigs from "./pages/Gigs"
+import Add from "./pages/Add"
+import Message from "./pages/Message"
+import Messages from "./pages/Messages"
 
 const App = () => {
   return (
     <div className="">
-      <Navbar/>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/gig" element={<Gig />} />
+          <Route exact path="/gigs" element={<Gigs/>} />
+          <Route exact path="/add" element={<Add/>} />
+          <Route exact path="/message" element={<Message/>} />
+          <Route exact path="/messages" element={<Messages/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
